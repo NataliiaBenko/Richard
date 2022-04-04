14de5c839225036d23bc47979ced857d53b49b7a
NataliiaBenko <nkraveta86@gmail.com>
2022-04-04 21:13:00 +0300

    menu

--- js.js
+++ js.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+
+
+
 console.clear();
 
 const nav = document.querySelector("nav");
